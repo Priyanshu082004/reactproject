b1b5a8812ecbbf47f1946ef9ea03a5401677f11c
Priyanshu082004 <priyanshuu202004@gmail.com>
2026-01-16 02:01:34 +0530

    REDUX TODO

---
+++ reduxtoolkit/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Addtodo from './components/Addtodo'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      REDUX TOOLKIT 
+    </h1>
+    <Addtodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
